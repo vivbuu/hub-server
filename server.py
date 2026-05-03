cb345ebafa70d510f40c3d2b48386bdcb8dba0c0
vivbuu <vipcompany8@ya.ru>
2026-05-03 09:48:39 +0300

    server.py

--- server.py
+++ server.py
@@ -6,14 +6,12 @@ app = Flask(__name__, static_url_path='', static_folder='.')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'secret!')
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 
-
 rooms = {
     'Общая': '',
     'С паролем': '123',
     'Третья': 'qwerty'
 }
 
-# Хранилище сообщений (по комнатам)
 history = {}
 
 @app.route('/')
@@ -36,10 +34,9 @@ def handle_join(data):
     
     join_room(room)
     
-    # Отправляем историю новому пользователю
     if room in history:
         for msg in history[room]:
-               emit('message', {'type': 'system', 'text': f'Комната "{room}" не существует'})
+            emit('message', msg)
     
     emit('message', {'type': 'system', 'text': f'{name} вошёл в комнату "{room}"'}, to=room)
 
@@ -47,14 +44,13 @@ def handle_join(data):
 def handle_message(msg):
     room = msg.get('room', 'Общая')
     
-    # Сохраняем в историю (последние 100 сообщений)
     if room not in history:
         history[room] = []
     history[room].append(msg)
     if len(history[room]) > 100:
         history[room] = history[room][-100:]
     
-        emit('message', msg, to=room)
+    send(msg, to=room)
 
 @socketio.on('clear')
 def handle_clear(data):
